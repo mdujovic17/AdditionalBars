2399f5ea988ecef76018146a9b36683b0388b113
Marko Dujović <dujovicm@outlook.com>
2021-08-19 12:44:05 +0200

    API Boilerplate

--- src/main/java/com/codenamerevy/additionalbars/api/block/CustomOxidizableBlock.java
+++ src/main/java/com/codenamerevy/additionalbars/api/block/CustomOxidizableBlock.java
@@ -4,16 +4,21 @@ import com.codenamerevy.additionalbars.common.registry.ABBlocks;
 import com.google.common.base.Suppliers;
 import com.google.common.collect.BiMap;
 import com.google.common.collect.ImmutableBiMap;
+import com.google.common.collect.ImmutableMap;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.ChangeOverTimeBlock;
 import net.minecraft.world.level.block.state.BlockState;
 
+import java.util.Collection;
+import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 import java.util.function.Supplier;
 
 public interface CustomOxidizableBlock extends ChangeOverTimeBlock<CustomOxidizableBlock.OxidizationState> {
 
-    Supplier<BiMap<Block, Block>> OXIDIZABLE_INCREASE = Suppliers.memoize(() -> ImmutableBiMap.<Block, Block>builder()
+    Map<Block, Block> OXIDIZE_UP = new ImmutableMap.Builder<Block, Block>().build();
+    Supplier<BiMap<Block, Block>> OXIDIZE_INCREASE = Suppliers.memoize(() -> ImmutableBiMap.<Block, Block>builder()
             .put(ABBlocks.COPPER_BARS.get(), ABBlocks.EXPOSED_COPPER_BARS.get())
             .put(ABBlocks.EXPOSED_COPPER_BARS.get(), ABBlocks.WEATHERED_COPPER_BARS.get())
             .put(ABBlocks.WEATHERED_COPPER_BARS.get(), ABBlocks.OXIDIZED_COPPER_BARS.get())
@@ -30,16 +35,16 @@ public interface CustomOxidizableBlock extends ChangeOverTimeBlock<CustomOxidiza
             .put(ABBlocks.HORIZONTAL_CROSSED_EXPOSED_COPPER_BARS.get(), ABBlocks.HORIZONTAL_CROSSED_WEATHERED_COPPER_BARS.get())
             .put(ABBlocks.HORIZONTAL_CROSSED_WEATHERED_COPPER_BARS.get(), ABBlocks.HORIZONTAL_CROSSED_OXIDIZED_COPPER_BARS.get())
             .build());
-    Supplier<BiMap<Block, Block>> OXIDIZABLE_DECREASE = Suppliers.memoize(() -> OXIDIZABLE_INCREASE.get().inverse());
+    Supplier<BiMap<Block, Block>> OXIDIZE_DECREASE = Suppliers.memoize(() -> OXIDIZE_INCREASE.get().inverse());
 
     static Optional<Block> getPrevious(Block block) {
-        return Optional.ofNullable(OXIDIZABLE_DECREASE.get().get(block));
+        return Optional.ofNullable(OXIDIZE_DECREASE.get().get(block));
     }
 
     static Block getFirst(Block firstBlock) {
         Block block = firstBlock;
 
-        for(Block block1 = OXIDIZABLE_DECREASE.get().get(firstBlock); block1 != null; block1 = OXIDIZABLE_DECREASE.get().get(block1)) {
+        for(Block block1 = OXIDIZE_DECREASE.get().get(firstBlock); block1 != null; block1 = OXIDIZE_DECREASE.get().get(block1)) {
             block = block1;
         }
 
@@ -51,7 +56,7 @@ public interface CustomOxidizableBlock extends ChangeOverTimeBlock<CustomOxidiza
     }
 
     static Optional<Block> getNext(Block block) {
-        return Optional.ofNullable(OXIDIZABLE_INCREASE.get().get(block));
+        return Optional.ofNullable(OXIDIZE_INCREASE.get().get(block));
     }
 
     static BlockState getFirst(BlockState state) {
